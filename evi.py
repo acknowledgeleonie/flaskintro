175500d71658b286f1b0e5f8c71d2fa296f6a05a
Evi Swinkels <EviSwinkels@Acknowledge.nl>
2022-12-16 16:13:01 +0100

    nieuwe functie

--- evi.py
+++ evi.py
@@ -1,2 +1,28 @@
+import mariadb
+import json
+
+class persoonsgegevensq:
+  voornaam = 'nvt'
+
 def methodevanevi():
+  mydb = mariadb.connect(
+      host='localhost',
+      username='root',
+      password = '',
+      database = 'personen'
+
+    )
+
+  cursor = mydb.cursor()
+  query = "SELECT * FROM persoonsgegevens"
+  cursor.execute(query) 
+  persoonsgegevens = cursor.fetchall()
+  persoon10 = persoonsgegevensq()
+  persoon10.voornaam = persoonsgegevens[0][1]
+  return json.dumps(persoon10.__dict__)
+
+x = methodevanevi()
+print (x)
+
+def methodevanevi_1():
   return "Hallo, Evi hier."
