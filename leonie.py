d825227f47180f2e911bdf1bc0d46eeb5dabc34f
Leonie van de Ven <LeonievandeVen@Acknowledge.nl>
2022-12-21 14:37:08 +0100

    kleine aanpassing

--- leonie.py
+++ leonie.py
@@ -1,2 +1,30 @@
+import mariadb
+import json
+
 def methodevanleonie():
-    return "Hoi allemaal groetjes van Leonie"
+
+    class Fiets:
+        merk = "nvt"
+
+    mydb = mariadb.connect(
+        host =
+        username =
+        password =
+        database=
+    )
+
+    cursor = mydb.cursor()
+    query = "SELECT * FROM fietsen"
+    cursor.execute(query)
+    fietsen = cursor.fetchall()
+    #print(fietsen)
+
+    fiets1 = Fiets()
+    fiets1.merk = fietsen[0][1]
+    print(fietsen)
+
+    return json.dumps(fiets1.__dict__)
+
+
+x = methodevanleonie()
+print(x)
