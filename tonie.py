968b1f76e2d6acc2b76624aaa2d30fd4195c8256
Tonie Bruijns <TonieBruijns@Acknowledge.nl>
2022-12-21 14:46:44 +0100

    updates

--- tonie.py
+++ tonie.py
@@ -1,2 +1,24 @@
+import mariadb
+import json
+import pandas as pd 
+
+class Fiets:
+    merk = 'nvt'
+
+
 def methodevantonie():
-	return "Dit is de return van Tonie"
+	mydb = mariadb.connect(
+    	host='localhost',
+		username='root',
+		password='',
+		database='demo'
+  )
+ 
+	cursor = mydb.cursor()
+	query = "SELECT * FROM fiets"
+	cursor.execute(query)
+	fietsen = cursor.fetchall()
+	fiets1 = Fiets()
+	fiets1.merk = fietsen[0][1]
+	return json.dumps(fiets1.__dict__)
+
